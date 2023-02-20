bf581073540237fea4471e8cd913f21a3dc1fdcf
jimitas <nonajun805@gmail.com>
2023-02-20 20:33:22 +0900

    ライトモード　ダークモードの実装

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', //ダークモードを有効化(ON/OFFの切り替え)
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
